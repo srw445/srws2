78cd082593f9df13cb9d3683d758a65a78a2edba
srw445 <206665451+srw445@users.noreply.github.com>
2026-05-06 16:22:04 +0900

    wsテストコード

--- php/ws/app/controllers/AssetSummaryController.php
+++ php/ws/app/controllers/AssetSummaryController.php
@@ -11,7 +11,12 @@ class AssetSummaryController {
     public function index() {
         $pdo = Database::getInstance();
         $assets = AssetSummary::getAll($pdo);
+        // 最大履歴番号を取得
         $historyNo = isset($_GET['history_no']) ? $_GET['history_no'] : null;
+        if ($historyNo === null && !empty($assets)) {
+            // $assetsは履歴番号降順なので先頭が最大
+            $historyNo = $assets[0]['履歴番号'];
+        }
         $assetDetails = AssetDetail::getAll($pdo, $historyNo);
         $assetRatios = AssetRatio::getAll($pdo, $historyNo);
         $assetRatioCountries = AssetRatioCountry::getAll($pdo, $historyNo);
